7f5791f22e2cca46c344fb39a1474e63e8fa2ef0
Korey Cheshire <korey.cheshire@gmail.com>
2021-10-06 08:41:30 -0700

    added licensing badge

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# a
+# qwer
+  ![Github License](https://img.shields.io/badge/license-GPL_3.0-green)
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Questions](#questions)
+* [License](#license)
 ## Description
-a
+ewrty
 ## Installation
-a
+fghj
 ## Usage
-a
-
-## Github Username
-d
-## Email Address
-d
-
+asdfg
+##License
+GPL_3.0
+## Questions?
+https://github.com/gfdsa/repo.git <br>
+Reach me by email at <br> gfds
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,13 +5,19 @@ function renderLicenseBadge(license) {
     return ""
     
   } else {
-    return "hello"
+    return `![Github License](https://img.shields.io/badge/license-${license}-green)`;
   }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "none"){
+    return ""
+  } else {
+    return `* [License](#license)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -19,7 +25,7 @@ function renderLicenseSection(license) {
   if(license === "none"){
     return ""
   } else {
-    return `## License
+    return `##License
 ${license}`
     
   }
@@ -28,6 +34,14 @@ ${license}`
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Questions](#questions)
+${renderLicenseLink(data.license)}
 ## Description
 ${data.description}
 ## Installation
@@ -35,11 +49,9 @@ ${data.installation}
 ## Usage
 ${data.usage}
 ${renderLicenseSection(data.license)}
-## Github Username
-${data.username}
-## Email Address
-${data.email}
-
+## Questions?
+https://github.com/${data.username}/repo.git <br>
+Reach me by email at <br> ${data.email}
 `;
 }
 
